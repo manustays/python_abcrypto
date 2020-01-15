d3e1f7328ab9b3435015192b6c1dce8cd2b3a525
Kumar Abhishek <manustays@gmail.com>
2020-01-15 20:49:13 +0530

    Set utility folder as a module & refactored code

--- blockchain.py
+++ blockchain.py
@@ -3,10 +3,10 @@ from functools import reduce
 # import json
 import pickle
 
-from hash_util import hash_block
 from block import Block
 from transaction import Transaction
-from verification import Verification
+from utility.hash_util import hash_block
+from utility.verification import Verification
 
 
 # Configure Reward received for a successful mining of a block
--- node.py
+++ node.py
@@ -3,7 +3,7 @@
 # from uuid import uuid4
 
 from blockchain import Blockchain
-from verification import Verification
+from utility.verification import Verification
 
 
 class Node:
